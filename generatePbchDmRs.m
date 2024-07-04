c34f0363e3a15cf752648aa7e2623043b7f5e79c
D.mon 24 <dchaminov@ya.ru>
2024-07-04 11:50:17 +0300

    Fixed seq. length

--- generatePbchDmRs.m
+++ generatePbchDmRs.m
@@ -4,17 +4,19 @@ arguments
     NCellId
 end
     cinit=2^11*(issb_+1)*(floor(NCellId/4)+1)+2^6*(issb_+2)+mod(NCellId,4);
-    initArr=int2bit(cinit,31,false).';
+    initArr=int2bit(cinit,5,false).';
 
-    x1=mSequence(31,[1,zeros(30)],[0,1,0,0,1,zeros(27)]);
-    x2=mSequence(31,initArr,[0,1,1,1,1,zeros(27)]);
+    x1=mSequence(5,[1,zeros(1,4)],[0,1,0,0,1,0]);
+    x2=mSequence(5,initArr,[0,1,1,1,1,0]);
     
     x1=circshift(x1,1600);
     x2=circshift(x2,1600);
 
     c=xor(x1,x2);
-
-    dmrs=1/sqrt(2)*(1-2*c(1:2:end)+1j-2j*c(2:2:end));
+    dmrs=zeros(1,81);
+    for k=1:81
+        dmrs(k)=1/sqrt(2)*(1-2*c(mod(2*k,31)+1)+1j-2j*c(mod(2*k+1,31)+1));
+    end
 end
 
 
