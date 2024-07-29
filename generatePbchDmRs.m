3f279d2bb4f5db70875dc0e5fb373a69a5797c8a
D.mon 24 <dchaminov@ya.ru>
2024-07-29 14:55:40 +0300

    new generation (nrPBCHDMRS matches)

--- generatePbchDmRs.m
+++ generatePbchDmRs.m
@@ -4,20 +4,28 @@ arguments
     issb_ % issb+is_second_hf*4
     NCellId
 end
-    cinit=2^11*(issb_+1)*(floor(NCellId/4)+1)+2^6*(issb_+2)+mod(NCellId,4);
-    initArr=int2bit(cinit,5,false).';
-
-    x1=mSequence(5,[1,zeros(1,4)],[0,1,0,0,1,0]);
-    x2=mSequence(5,initArr,[0,1,1,1,1,0]);
-    
-    x1=circshift(x1,1600);
-    x2=circshift(x2,1600);
-
-    c=xor(x1,x2);
-    dmrs=zeros(1,81);
-    for k=1:81
-        dmrs(k)=1/sqrt(2)*(1-2*c(mod(2*k,31)+1)+1j-2j*c(mod(2*k+1,31)+1));
+    cinit=2^11*(issb_+1)*(floor(NCellId/4)+1)+2^6*(issb_+1)+mod(NCellId,4);
+    w2=int2bit(cinit,31,false).';
+    w1=[1,zeros(1,30)];
+    for i=32:1600
+        nw1=mod(w1(1)+w1(4),2);
+        nw2=mod(w2(4)+w2(3)+w2(2)+w2(1),2);
+        w1=circshift(w1,-1);
+        w1(end)=nw1;
+        w2=circshift(w2,-1);
+        w2(end)=nw2;
     end
+    c=zeros(1,288);
+    for i=1:288
+        nw1=mod(w1(1)+w1(4),2);
+        nw2=mod(w2(4)+w2(3)+w2(2)+w2(1),2);
+        w1=circshift(w1,-1);
+        w1(end)=nw1;
+        w2=circshift(w2,-1);
+        w2(end)=nw2;
+        c(i)=mod(nw1+nw2,2);
+    end
+    dmrs=1/sqrt(2)*(1-2*c(1:2:end))+1j/sqrt(2)*(1-2*c(2:2:end));
 end
 
 
